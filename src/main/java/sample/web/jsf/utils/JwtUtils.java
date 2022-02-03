9e94c1046b519f1dd127f17768b3efc0726338b8
Adam Kapuściński <ziemniaczek320@gmail.com>
2022-02-03 01:22:55 +0100

    dodanie stuffu

--- src/main/java/sample/web/jsf/utils/JwtUtils.java
+++ src/main/java/sample/web/jsf/utils/JwtUtils.java
@@ -18,4 +18,26 @@ public class JwtUtils {
         }
     }
 
+    public static String getUserRole() {
+        String token = RestClient.getJWT();
+        token = token.replace("Bearer ", "");
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(token);
+            return signedJWT.getJWTClaimsSet().getStringClaim("permissionLevel");
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static String getUserName() {
+        String token = RestClient.getJWT();
+        token = token.replace("Bearer ", "");
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(token);
+            return signedJWT.getJWTClaimsSet().getSubject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
 }
